2a263bbc7fac6f7fea71957a937c0f478215ec8b
mykel bwan <mykelbwan@gmail.com>
2026-05-29 20:52:43 +0100

    prod fix

--- don/main.py
+++ don/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 
+from api import app
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=3003, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=3003, reload=False)
